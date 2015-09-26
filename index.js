9275ac5d6b47efdd5975548ec5c1cf5c5ca76a84
Nikolay Bachiyski <nb@nikolay.bg>
2015-09-26 05:13:22 -0500

    Follow WordPress’ coding standards

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var assert = require('better-assert');
+var assert = require( 'better-assert' );
 
-describe('Numbers', function() {
-	describe('Integers', function() {
-		it('1 should equal 1', function() {
-			assert(1 === 1);
-		});
+describe( 'Numbers', function() {
+	describe( 'Integers', function() {
+		it( '1 should equal 1', function() {
+			assert( 1 === 1 );
+		} );
 
-		it('2 should be larger than 1', function() {
-			assert(2 > 1);
-		});
-	});
-});
+		it( '2 should be larger than 1', function() {
+			assert( 2 > 1 );
+		} );
+	} );
+} );
