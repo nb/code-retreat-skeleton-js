2cd0255b5aadaec3ebedcf2484db80e84f28afc9
Nikolay Bachiyski <nb@nikolay.bg>
2016-01-23 09:23:00 +0200

    ES6 :-)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var assert = require( 'chai' ).assert, // assert reference: http://chaijs.com/api/assert/
-	_ = require( 'lodash' ); // handy utilities: https://lodash.com/
+import _ from 'lodash';
+import { assert } from 'chai'; // assert reference: http://chaijs.com/api/assert/
 
-describe( 'Numbers', function() {
-	describe( 'Sample', function() {
-		it( '2 should be larger than 1', function() {
+describe( 'Numbers', () => {
+	describe( 'Sample', () => {
+		it( '2 should be larger than 1', () => {
 			assert( 2 > 1 );
 		} );
 
-		it( '1 should equal 1', function() {
+		it( '1 should equal 1', () => {
 			assert.equal( 1, 1 );
 		} );
 
-		it( 'those two arrays should be equal', function() {
+		it( 'those two arrays should be equal', () => {
 			assert.deepEqual( [1, 2, 3], [1, 2, 3] );
 		} );
 	} );
