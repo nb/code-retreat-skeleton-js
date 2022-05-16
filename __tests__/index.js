26e8b8b48f649e1b408b7236ff6a842df6acb209
Brad Griffith <brad.griffith@automattic.com>
2022-05-16 15:02:29 -0500

    Switch to jest. Drop babel.

---
+++ __tests__/index.js
@@ -0,0 +1,12 @@
+test( '2 should be larger than 1', () => {
+    expect( 2 ).toBeGreaterThan( 1 );
+} );
+
+test( '1 should equal 1', () => {
+    expect( 1 ).toEqual( 1 );
+} );
+
+test( 'those two arrays should be equal', () => {
+    expect( [1, 2, 3] ).toEqual( [1, 2, 3] );
+} );
+
