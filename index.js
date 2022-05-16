c804c9de9f97fef8e7291a504929938376317459
Brad Griffith <brad.griffith@automattic.com>
2022-05-16 15:23:09 -0500

    Group tests in `describe` and add note about implementation of Game of Life.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-test( '2 should be larger than 1', () => {
-    expect( 2 ).toBeGreaterThan( 1 );
-} );
+// Implement Game of Life here
 
-test( '1 should equal 1', () => {
-    expect( 1 ).toEqual( 1 );
-} );
+describe( 'The Game of Life', () => {
+    test( '2 should be larger than 1', () => {
+        expect( 2 ).toBeGreaterThan( 1 );
+    } );
 
-test( 'those two arrays should be equal', () => {
-    expect( [1, 2, 3] ).toEqual( [1, 2, 3] );
-} );
+    test( '1 should equal 1', () => {
+        expect( 1 ).toEqual( 1 );
+    } );
 
+    test( 'those two arrays should be equal', () => {
+        expect( [1, 2, 3] ).toEqual( [1, 2, 3] );
+    } );
+} );
